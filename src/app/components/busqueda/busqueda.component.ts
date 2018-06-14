4b6f2a3f1b7f740d53b8201b555d27042776f97e
Jorge Zarate <jorgezarate@gmail.com>
2018-06-13 21:44:20 -0300

    Creacion de Componente busqueda

---
+++ src/app/components/busqueda/busqueda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent {
+
+
+}
